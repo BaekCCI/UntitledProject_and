beeec820c7760a1c050115118f8ad6f02bb33142
baekcii <bsy8470@naver.com>
2025-08-05 00:49:38 +0900

    💄[Design] 모집 기간 선택 다이얼로그 UI 구현

--- app/src/main/java/com/baek/untitledproject/ui/board/write/RecruitDateSelectDialogFragment.kt
+++ app/src/main/java/com/baek/untitledproject/ui/board/write/RecruitDateSelectDialogFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.baek.untitledproject.R
-import com.baek.untitledproject.databinding.FragmentInfoWriteBinding
+import com.baek.untitledproject.databinding.FragmentRecruitDateSelectDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.datepicker.MaterialDatePicker
 
-class InfoWriteFragment : Fragment() {
+class RecruitDateSelectDialogFragment : BottomSheetDialogFragment() {
 
-    private var _binding: FragmentInfoWriteBinding? = null
+    private var _binding: FragmentRecruitDateSelectDialogBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,20 +23,13 @@ class InfoWriteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentInfoWriteBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentRecruitDateSelectDialogBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setUpRecruitDateSelectBtn()
-    }
-
-    private fun setUpRecruitDateSelectBtn() {
-        binding.recruitDateSelectBtn.setOnClickListener {
-            RecruitDateSelectDialogFragment().show(parentFragmentManager, "RecruitDateDialog")
-        }
     }
 
     override fun onDestroyView() {
