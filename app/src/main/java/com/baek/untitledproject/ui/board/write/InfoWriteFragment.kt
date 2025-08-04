ea3e1c6aaaa4f4f8493c2619b7dfaf0ef6706610
baekcii <bsy8470@naver.com>
2025-08-05 00:27:11 +0900

    💄[Design] 공고 상세정보 입력 UI 구현 및 네비게이션 추가

---
+++ app/src/main/java/com/baek/untitledproject/ui/board/write/InfoWriteFragment.kt
@@ -0,0 +1,37 @@
+package com.baek.untitledproject.ui.board.write
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.baek.untitledproject.R
+import com.baek.untitledproject.databinding.FragmentInfoWriteBinding
+
+class InfoWriteFragment : Fragment() {
+
+    private var _binding: FragmentInfoWriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInfoWriteBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
