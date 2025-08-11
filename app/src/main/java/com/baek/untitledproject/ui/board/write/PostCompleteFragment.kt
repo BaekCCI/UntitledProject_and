0f3e342a379989e2f88e8a57f91c060c435fc41f
baekcii <bsy8470@naver.com>
2025-08-11 23:18:22 +0900

    ✨[Feat] 버튼 클릭시 네비게이션 설정

--- app/src/main/java/com/baek/untitledproject/ui/board/write/PostCompleteFragment.kt
+++ app/src/main/java/com/baek/untitledproject/ui/board/write/PostCompleteFragment.kt
@@ -5,56 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.baek.untitledproject.R
+import com.baek.untitledproject.databinding.FragmentPostCompleteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PostCompleteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PostCompleteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentPostCompleteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_complete, container, false)
+    ): View {
+        _binding = FragmentPostCompleteBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostCompleteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostCompleteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toHomeBtn.setOnClickListener {
+            findNavController().navigate(
+                R.id.boardFragment,
+                null,
+                navOptions {
+                    popUpTo(R.id.write_board_nav_graph) { inclusive = true }
+                    launchSingleTop = true
                 }
-            }
+            )
+        }
+        binding.toMyPostBtn.setOnClickListener {
+            findNavController().navigate(
+                R.id.boardFragment, //TODO: 내 공고페이지로 변경
+                null,
+                navOptions {
+                    popUpTo(R.id.write_board_nav_graph) { inclusive = true }
+                    launchSingleTop = true
+                }
+            )
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
