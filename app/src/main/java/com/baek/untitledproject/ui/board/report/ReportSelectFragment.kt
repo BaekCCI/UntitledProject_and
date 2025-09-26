a160009c49888b3ad296b6baacab64ab1d2ca802
baekcii <bsy8470@naver.com>
2025-09-26 23:45:46 +0900

    💄[Design] 신고하기 UI 구현

---
+++ app/src/main/java/com/baek/untitledproject/ui/board/report/ReportSelectFragment.kt
@@ -0,0 +1,19 @@
+package com.baek.untitledproject.ui.board.report
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.baek.untitledproject.R
+
+class ReportSelectFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_report_select, container, false)
+    }
+}
---
+++ app/src/main/java/com/baek/untitledproject/ui/board/report/ReportWriteFragment.kt
@@ -0,0 +1,20 @@
+package com.baek.untitledproject.ui.board.report
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.baek.untitledproject.R
+
+class ReportWriteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_report_write, container, false)
+    }
+
+}
